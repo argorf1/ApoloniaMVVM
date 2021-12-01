56c72c9d05b9d547dd30b756be70b6b17b01222f
argorf1 <jpsotohidalgo@hotmail.com>
2021-12-01 18:11:48 -0300

    Validation Service
    Duracion debe ser mayor a 0

--- ApoloniaApp/Services/ValidationService.cs
+++ ApoloniaApp/Services/ValidationService.cs
@@ -11,10 +11,22 @@ namespace ApoloniaApp.Services
     {
         #region Validaciones
 
+        static Dictionary<int, string> _validField = new Dictionary<int, string>()
+        {
+            {0, "/Assets/Icons/ico_required.png"}
+            ,{1, "/Assets/Icons/ico_valid.png"}
+            ,{2, "/Assets/Icons/icon_invalid.png"}
+        };
+
+
+        public static string Image(bool initialized, bool valid)
+        {
+            return initialized ? (valid ? _validField[1]:_validField[2]):_validField[0];
+        }
         public static bool Text(string text) => !string.IsNullOrEmpty(text);
         public static bool Nombre(string text) => Text(text) && !Regex.IsMatch(text, @"[0-9]+");
         public static bool PhoneNumber(string text) => text.Length >= 8;
-        public static bool Number(int number) => number >= 0;
+        public static bool Number(int number) => number >= 1;
         public static bool ListContent<TModel>(IEnumerable<TModel> lista) where TModel : ModelBase => lista.Any();
         public static bool Email(string email) => Text(email) && Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
         public static bool Run(string run) => Text(run) && (ValidateRunService.ValidaRut(run)|| run == "2");
